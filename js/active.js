3bef25599556a5620e717e7a141f8f3a9cfc3db1
Bashir Hamza <bashirhamza05@gmail.com>
2021-07-12 08:59:23 +0100

    Made the select update and responsive tables

--- js/active.js
+++ js/active.js
@@ -111,4 +111,19 @@
         }
     });
 
+    $(".tf-custom-select .tf-select-input-box input").on("click", function () {
+        if ($('.tf-custom-select .select-options').hasClass("open")) {
+            $('.tf-custom-select .select-options').removeClass("open");
+        } else {
+            $('.tf-custom-select .select-options').addClass("open");
+        }
+    });
+    $(".tf-custom-select .select-options a").on("click", function (e) {
+        e.preventDefault();
+        let selectValue = $(this).text();
+        $('.tf-custom-select .tf-select-input-box input').val(selectValue);
+        $('.tf-custom-select .select-options').removeClass("open");
+    });
+
+
 })(jQuery);
